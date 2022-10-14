a2b03ace9a5adb5d463910429e88c2544f77a997
kblalock22 <115417993+kblalock22@users.noreply.github.com>
2022-10-14 18:50:03 -0500

    Create MadLibs_Game.py

---
+++ .github/MadLibs_Game.py
@@ -0,0 +1,39 @@
+#This is an interactive MadLibs game in Python!
+
+adj1 = str(input("Enter an adjective: "))
+
+animal1 = str(input("Enter a singular animal: "))
+
+water1 = str(input("Enter a singular body of water: "))
+
+verb_ing1 = str(input("Enter a verb ending in -ing: "))
+
+verb_ed1 = str(input("Enter a verb ending in -ed: "))
+
+noun1 = str(input("Enter a noun: "))
+
+food1 = str(input("Enter a plural food: "))
+
+adj2 = str(input("Enter another adjective: "))
+
+verb_inf1 = str(input("Enter a verb in the infinitive form: "))
+
+name1 = str(input("Enter a name: "))
+
+adj3 = str(input("Enter another adjective: "))
+
+
+print("Today, the " + adj1 + " " + animal1 + " crossed the " + water1 + ".")
+
+print("Then, the " + animal1 + " started " + verb_ing1 + ".")
+
+print("Because of this, the " + animal1+ " almost " + verb_ed1 + " but was thankfully saved by the large " + noun1 + " that smelled like " + food1 + ".")
+
+print("Because the " + noun1 + " smelled like " + food1 + ", the " + animal1 + " became very hungry and " + adj2 + ".")
+
+print("The " + animal1 + " was deciding to either eat or " + verb_inf1 + " " + name1 + ", the " + noun1 + ".")
+
+print("The " + animal1 + " decided to eat " + name1 + ".")
+
+print(name1 + " was very crunchy and " + adj3 + ".")
+
